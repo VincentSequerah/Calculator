fbfa3ed50c7e760c34c4cc57cef19aa306657a22
VincentSequerah <v1jnicholas@gmail.com>
2024-09-10 18:12:21 +0700

    finished

--- main.js
+++ main.js
@@ -1,34 +1,24 @@
-function Solve(val) {
-    var v = document.getElementById('output');
-    v.value += val;
+const display = document.getElementById('output');
+
+
+function append(input){
+    display.value += input;
  }
- function Answer() {
-    var num1 = document.getElementById('output').value;
+
+function cle(){
+   display.value = '';
+}
+
+function del(){
+   display.value = display.value.slice(0, -1);
+}
+
+
+
+function answer() {
     try {
-       var num2 = eval(num1.replace('x', '*'));
-       document.getElementById('output').value = num2;
+      display.value = eval(display.value.replace('x', '*'));
     } catch {
-       document.getElementById('output').value = 'Error';
+       display.value = 'Error';
     }
  }
- function Clear() {
-    var inp = document.getElementById('output');
-    inp.value = '';
- }
- function Del() {
-    var ev = document.getElementById('output');
-    ev.value = ev.value.slice(0, -1);
- }
- document.addEventListener('keydown', function (event) {
-    const key = event.key;
-    const validKeys = '0123456789+-*/.%';
-    if (validKeys.includes(key)) {
-       Solve(key === '*' ? 'x' : key);
-    } else if (key === 'Enter') {
-       Result();
-    } else if (key === 'Backspace') {
-       Back();
-    } else if (key.toLowerCase() === 'c') {
-       Clear();
-    }
- });
